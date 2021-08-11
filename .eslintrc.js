0e50495b7002c3fcbcbf646d2697e2de4c3a6b1b
Andre van Tonder <drevan@hey.com>
2021-08-11 20:32:06 +1000

    Prepare for testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
-export default {
+module.exports = {
   root: true,
   env: {
     browser: true,
+    node: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+    'plugin:jest-dom/recommended',
+  ],
+  plugins: ['jest-dom'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
